e55705fc9e40c80b5de0b93713717a719b96edb8
Bohdan Forostianyi <qwebeck5@gmail.com>
2019-11-19 03:52:27 +0100

    Finished script and analysis.

--- Lab2/LabScript.m
+++ Lab2/LabScript.m
@@ -1,21 +1,117 @@
 WPC = [0;0];
-
-P = eye(2);
-J=[-2-2*i,0;0,-2+2*i];
-A = P * J * P^(-1);
-
-
+A = [0,0;0,0];
 figure('Renderer', 'painters', 'Position', [10 10 900 600])
 open('Lab2');
+% Zmienna od ktorej zalezy czy wartosci wlasne beda wybierany z brzegu
+% kwadartu, czy z brzegu kola 
+% Mozna wybierac jednoczesnie z brezgu kwadratu i z brzegu kola. W tym celu
+% nalezy rozkomentowac obie opcje
+
+InitialSystemState = 'Circle'
+% InitialSystemState = 'Square'
+%------------------------------------------------------------------------
+% Ustawienie P jako macierze jednostkowej jest potrzebne do realizacji
+% pierwszej czesci czwiczenia
+P = eye(2);
+
+% 1 Liniowy dzielnik elementarny, podwojna wartosc wlasna, rowna 0 
+
+J = [0,0;0,0];
+A = P * J * P^(-1);
 drawData
 
-% 7 Eigenvalues on different sides of 0 on real axes 
+% Widzimy system stabilny. Dzije sie tak latego, ze w rownaniu stanu,
+% opisanego za pomoca macierzy nie mamy zadnych wspolczynnikow oprocz zera.
+% Czyli pochodna pierwsza jest rowna 0, druga tez => x jest stala, dlatego
+% po usunieciu osi czasu mamy puntk na plasczyznie, ktory odpowiada
+% poczatku ukladu
+
+% 2 Podwojna wartosc wlasna rowna zeru, nieliniowy dzielnik elementarny 
+J = [0,1;0,0];
+A = P * J * P^(-1);
+drawData
+% Widzimy system niestabilny. Trajektorii nie zbiegaja do jedengo puntktu,
+% a rozchodza sie od zrodla. 
+
+% 3 Zerowa wartosc wlasna oraz wartosc wlasna mniejsza od zera
+J = diag([-1, 0]);
+A = P * J * P^(-1);
+drawData
+% Widzimy system asymptotycznie stabilny, dlatego ze trajektorii zbiegaja
+% sie do jednego puntku, jednak ten puntk jest rozny dla kazdej z
+% trajektorii
+
+% 4 Obie wartosci wlasne sa mniejsze od zera
+J = diag([-5, -1]);
+A = P * J * P^(-1);
+drawData
+% Widzimy system stabilny. Jedno zrolo
+
+% 5 Podwojna wartosc wlasna mniejsza od 0, liniowy dzielnik elementarny
+J = [-1,0;0,-1];
+A = P * J * P^(-1);
+drawData
+% System stabilny: wiele trajektrorii jeden punkt do ktorego one zbiegaja 
+
+% 6 Podwojna wartosc wlasna mniejsza od 0, nieliniowy dzielnik elementarny
+J = [-1,1;0,-1];
+A = P * J * P^(-1);
+drawData
+% Rowniez system stabilny 
+
+% 7 Wartoscie wlasne ktore znajduja sie po rozne strony od 0
 J  = [ -1, 0;0, 1];
 A = P * J * P^(-1);
 drawData
+% System jest stabilny: kazda trajektoria zbiega do pewnej granicy
 
-% 8 Complex numbers 
-J  = [ 1i, 0;0, -1i];
-A = P * J * P^(-1);
+% 8 Zespolone wartosci wlasne, czesc rzeczywista jest rowna 0
+%J  = [ 1i, 0;0, -1i];
+%A = P * J * P^(-1);
+%drawData
+
+% 9 Wartosci wlasne - dwie spzezone liczbe urojone, mnijesze od zera 
+%J=[-2-2i,0;0,-2+2i];
+%A = P * J * P^(-1);
+%drawData
+J = [-1,1;0,-1];
+% Druga czesc cwiczenia. 
+% Badanie wplywu wartosci wlasnych 
+% Takie sam uklad wartosci wlascnych, ale one sa w rozny sposob
+% przeskalowane
+J = J * 2;
+A = P * J * P^(-1)
 drawData
+%
+J = J * 4;
+A = P * J * P^(-1)
+drawData
+% Widzimy, ze przy zmianie wektorow rosnie szybkosc z jaka nasz system day
+% do stanu stabilnosci ( czarwone strzalki sie wydluzyly )
+
+
+J = diag([-1, 0]);
+% Badanie wplywu wektorow wlasnych
+P = [1,0;0,1];
+A = P * J * P^(-1)
+drawData
+
+% Druga czesc cwiczenia. Rozwiazanie dla roznych wektorow wlasnych
+P = [1,1;0,1];
+A = P * J * P^(-1)
+drawData
+
+P = [1,-1;0,1];
+A = P * J * P^(-1)
+drawData
+% Zmieniac wektory glowne w macierzy P faktucznie zmieniamy baze pzestrzeni.
+% System jest jednoznacznie opisany przez macierz J, czyli macierz
+% stanu A ma takie same wlasnosci co macierz J. jak widzimy, po
+% przeksztalceniu wykres nadal ma taka sama stabilnosc i taki sam portret
+% fazowy, z tym ze portret jest zmieniony przekstalcony za pomoca wektorow
+% glownych. Prowadzi to do wniosku, ze system bedzie taki sam w kazdej
+% przestrzeni
+% Przemnozenie wektorow
+% wlasnych przez macierz J powoduje to, ze zmienia sie polozenie punktow w
+% zaleznosci od tego. W przypadku jezel B = 0
 
--- Lab2/drawData.m
+++ Lab2/drawData.m
@@ -1,23 +1,32 @@
     
     tiledlayout(1,1);
     clf
-    WPCval = linspace(0,2*pi,100);
+    
+    WPCval = linspace(0,2*pi,10);
     ax1 = nexttile;
-    for i = 1:length(WPCval)
+    if InitialSystemState == 'Circle'
+    for idx = 1:length(WPCval)
         res = sim('Lab2');
-        plot(res.x(:,1), res.x(:,2),'LineWidth', 0.2)   
+        plot(res.x(:,1), res.x(:,2),'k','LineWidth', 0.2)
+        % conditional to draw a single in case if all data will land in the
+        % same point. Should use this method, because with plot function one
+        % point will be unnoticalble
+        if max(abs(res.x(:,1) - max(res.x(:,1)))) <= eps && max(abs(res.x(:,2) - max(res.x(:,2)))) <= eps
+            plot(res.x(1,1), res.x(1,2), 'ko', 'MarkerSize', 5,'MarkerFaceColor','k');
+        end
+    
         xlabel('X1')
         ylabel('X2')
         % direction 
         hold on
-        p1 = [res.x(i,1) res.x(i,2)]
-        p2 = [res.x(i+20,1) res.x(i+20,2)]
+        p1 = [res.x(idx,1) res.x(idx,2)];
+        p2 = [res.x(idx+20,1) res.x(idx+20,2)];
         dp = p2 - p1;
-        quiver(p1(1),p1(2)+p1(2) * 0.2 * dp(2),dp(1),dp(2),'k')
+        quiver(p1(1),p1(2),dp(1),dp(2),'r')
        %-------------------------
         hold on 
-        WPC(1) = sin(WPCval(i));
-        WPC(2) = cos(WPCval(i));
+        WPC(1) = sin(WPCval(idx));
+        WPC(2) = cos(WPCval(idx));
     end
     % eigenvectors 
     quiver([0,0],[0,0],[P(1,2),P(1,1)],[P(2,2),P(2,1)],'b','LineWidth', 1)    
@@ -27,33 +36,44 @@
     grid
     title("WPC leza na okregu jednostkowym o srodku w (0,0)")
     axis square
-    
+    xlim([-1.2,1.2]);
+    ylim([-1.2,1.2]);
     %---------------
     disp("Press any key ....")
     pause;
+    end
     clf
     
+    if InitialSystemState == 'Square'
     % new figure 
     
-    for i = 1:length(WPCval)
-        WPC(1) = sin(WPCval(i));
-        WPC(2) = cos(WPCval(i));
+    for idx = 1:length(WPCval)
+        WPC(1) = sin(WPCval(idx));
+        WPC(2) = cos(WPCval(idx));
         p = max(abs(WPC(1)),abs(WPC(2)));
         WPC = WPC/p;
         res = sim('Lab2');
-        plot(res.x(:,1), res.x(:,2),'LineWidth', 0.2)
+        plot(res.x(:,1), res.x(:,2),'k','LineWidth', 0.2)
+        % conditional to draw a single in case if all data will land in the
+        % same point. Should use this method, because with plot function one
+        % point will be unnoticalble
+        if max(abs(res.x(:,1) - max(res.x(:,1)))) <= eps && max(abs(res.x(:,2) - max(res.x(:,2)))) <= eps
+             plot(res.x(1,1), res.x(1,2), 'ko', 'MarkerSize', 5,'MarkerFaceColor','k');
+        end
         xlabel('X1')
         ylabel('X2')
         hold on
         % direction 
-        p1 = [res.x(i,1) res.x(i,2)]
-        p2 = [res.x(i+20,1)  res.x(i+20,2)]
+        p1 = [res.x(idx,1) res.x(idx,2)];
+        p2 = [res.x(idx+20,1)  res.x(idx+20,2)];
         dp = p2 - p1;
-        quiver(p1(1) ,p1(2)+p1(2) * 0.2 * dp(2) ,dp(1),dp(2),'k')
+        quiver(p1(1) ,p1(2),dp(1),dp(2),'r')
         %-------------------------
         hold on
     end
     % eigenvectors
+    
+    
     quiver([0,0],[0,0],[P(1,2),P(1,1)],[P(2,2),P(2,1)],'b','LineWidth', 1)
     hold off
     %-------------------------
@@ -61,8 +81,9 @@
     grid
     title("WPC leza na brzegach kwadratu" )
     axis square
-    
+    xlim([-1.2,1.2]);
+    ylim([-1.2,1.2]);
     %-------------------------
     disp('Press any key to continue')
     pause
-    
+    end
