d19f6d099bb8d7d6f59aafa002a25abadd2471c7
Bohdan Forostianyi <qwebeck5@gmail.com>
2019-12-07 20:42:30 +0100

    Ad plot data

---
+++ Lab3/plotData.m
@@ -0,0 +1,18 @@
+function plotData(Simulation,A,IC)
+%Simulation, A, Time
+% Simulation - Obiekt z przeprowadzonej symulacji
+% A - macierz stanu, potrzebna dla wizualizacji rozwiazania ciaglego
+% ukladu)
+% IC - stan poczatkowy systemu
+scatter(Simulation.simout.Time,Simulation.simout.Data,'filled');
+hold on
+plot(Simulation.simout.Time,Simulation.simout.Data,'r');
+hold on
+Time = 0:0.1:10;
+plot(Time,exp(A*Time)*IC,'k');
+hold off
+grid
+legend('Wartosci x(t)','Interpolowana funkcja x(t)','Rzeczywista funkcja x(t)')
+disp('Press any key ...')
+pause;
+end
