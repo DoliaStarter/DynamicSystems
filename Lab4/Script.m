6f0bde68f82d6f7b3f1e7970ea17aa91bc4aaba8
Dushess0 <ilyabannila@gmail.com>
2019-12-14 22:31:15 +0100

    final version

--- Lab4/Script.m
+++ Lab4/Script.m
@@ -1,46 +1,8 @@
 clear all
 gcf = figure();
 set(gcf, 'Position',  [100, 100, 700, 700])
-Numerator=[3 3];
-Denominator=[1 2 21];
 
-%Zadanie 1
-SimTime = 10;
-FreqV = [1,5,10,15];
-PhV = [0,pi/3,pi/4,pi/2];
-AV =  [1,2,3,4];
-colors = ['r','g','b','k'];
-
-% Zmiana czestotliwosci
-disp('Chaging frequency')
-for i = 1:length(FreqV)
-    Freq = FreqV(i)
-    Ph = PhV(1)
-    A = AV(1)
-    a = sim('Lab_4');
-    plotDataTiled(a.sim_in,a.sim_out,'k','Zmiana czestotliwosci','Sygnal wejsciowy','Sygnal wyjsciowy');
-end
-%Zmiany fazy
-disp('Chaging phase')
-for i = 1:length(PhV)
-    Freq = FreqV(1);
-    Ph = PhV(i);
-    A = AV(1);
-    a = sim('Lab_4');
-    plotDataTiled(a.sim_in,a.sim_out,'k','Zmiana fazy','Sygnal wejsciowy','Sygnal wyjsciowy');
-end
-%Zmiany amplitudy
-disp('Chaging amplitude')
-for i = 1:length(AV)
-    Freq = FreqV(1);
-    Ph = PhV(1);
-    A = AV(i);
-    a = sim('Lab_4');
-    plotDataTiled(a.sim_in,a.sim_out,'k','Zmiana amplitudy','Sygnal wejsciowy','Sygnal wyjsciowy');
-end
-
-
-
+SimTime=10;
 
 
 
@@ -88,7 +50,7 @@ Freq = FreqV(i);
 Ph = PhV(i);
 A = AV(i);
 a = sim('Lab_4');
-plotData(a.sim_in,a.sim_out,'k',"Filter dolnoprzepustowy",'Sygnal wejsciowy','Sygnal wyjsciowy');
+plotData(a.sim_in,a.sim_out,'k',"Filter dolnoprzepustowy Czestotliwosc="+FreqV(i),'Sygnal wejsciowy','Sygnal wyjsciowy');
 end
 
 Numerator=[0.4 0]
@@ -98,7 +60,7 @@ Freq = FreqV(i);
 Ph = PhV(i);
 A = AV(i);
 a = sim('Lab_4');
-plotData(a.sim_in,a.sim_out,'k',"Filter gornoprzepustowy",'Sygnal wejsciowy','Sygnal wyjsciowy');
+plotData(a.sim_in,a.sim_out,'k',"Filter gornoprzepustowy Czestotliwosc="+FreqV(i),'Sygnal wejsciowy','Sygnal wyjsciowy');
 end
     
 Numerator=[1 0]
@@ -108,7 +70,7 @@ Freq = FreqV(i);
 Ph = PhV(i);
 A = AV(i);
 a = sim('Lab_4');
-plotData(a.sim_in,a.sim_out,'k',"Filter psamoprzepustowy",'Sygnal wejsciowy','Sygnal wyjsciowy');
+plotData(a.sim_in,a.sim_out,'k',"Filter psamoprzepustowy Czestotliwosc="+FreqV(i),'Sygnal wejsciowy','Sygnal wyjsciowy');
 end
     
 Numerator=[0 1]
@@ -118,7 +80,7 @@ Freq = FreqV(i);
 Ph = PhV(i);
 A = AV(i);
 a = sim('Lab_4');
-plotData(a.sim_in,a.sim_out,'k',"Filter drugiego rzedu",'Sygnal wejsciowy','Sygnal wyjsciowy');
+plotData(a.sim_in,a.sim_out,'k',"Filter drugiego rzedu Czestotliwosc="+FreqV(i),'Sygnal wejsciowy','Sygnal wyjsciowy');
 end
 
 
@@ -128,35 +90,80 @@ Numerator=[0 1]
 Denominator=[4 1]
 
 a = sim('SumFilter');
-plotDataTiled(a.sim_in,a.sim_out,'k',"Filter dolnoprzepustowy",'Sygnal wejsciowy','Sygnal wyjsciowy');
+plotDataTiled(a.sim_in,a.sim_out,'k',"Filter dolnoprzepustowy Czestotliwosc="+FreqV(i),'Sygnal wejsciowy','Sygnal wyjsciowy');
 
 Numerator=[0.4 0]
 Denominator=[0.4 1]
 
 a = sim('SumFilter');
-plotDataTiled(a.sim_in,a.sim_out,'k',"Filter gornoprzepustowy",'Sygnal wejsciowy','Sygnal wyjsciowy');
+plotDataTiled(a.sim_in,a.sim_out,'k',"Filter gornoprzepustowy Czestotliwosc="+FreqV(i),'Sygnal wejsciowy','Sygnal wyjsciowy');
    
 Numerator=[1 0]
 Denominator=[1 2 1]
 
 a = sim('SumFilter');
-plotDataTiled(a.sim_in,a.sim_out,'k',"Filter pasmoprzepustowy",'Sygnal wejsciowy','Sygnal wyjsciowy');
+plotDataTiled(a.sim_in,a.sim_out,'k',"Filter pasmoprzepustowy Czestotliwosc="+FreqV(i),'Sygnal wejsciowy','Sygnal wyjsciowy');
 
 Numerator=[0 1]
 Denominator=[1 0.1 1]
 a = sim('SumFilter');
-plotDataTiled(a.sim_in,a.sim_out,'k',"Filter drugiego rzedu",'Sygnal wejsciowy','Sygnal wyjsciowy');
+plotDataTiled(a.sim_in,a.sim_out,'k',"Filter drugiego rzedu Czestotliwosc="+FreqV(i),'Sygnal wejsciowy','Sygnal wyjsciowy');
+
+
+
+
+
+Numerator=[3 3];
+Denominator=[1 2 21];
+
+%Zadanie 1
+SimTime = 10;
+FreqV = [1,5,10,15];
+PhV = [0,pi/3,pi/4,pi/2];
+AV =  [1,2,3,4];
+colors = ['r','g','b','k'];
+
+% Zmiana czestotliwosci
+disp('Chaging frequency')
+for i = 1:length(FreqV)
+    Freq = FreqV(i)
+    Ph = PhV(1)
+    A = AV(1)
+    a = sim('Lab_4');
+    plotDataTiled(a.sim_in,a.sim_out,'k','Zmiana czestotliwosci','Sygnal wejsciowy','Sygnal wyjsciowy');
+end
+%Zmiany fazy
+disp('Chaging phase')
+for i = 1:length(PhV)
+    Freq = FreqV(1);
+    Ph = PhV(i);
+    A = AV(1);
+    a = sim('Lab_4');
+    plotDataTiled(a.sim_in,a.sim_out,'k','Zmiana fazy','Sygnal wejsciowy','Sygnal wyjsciowy');
+end
+%Zmiany amplitudy
+disp('Chaging amplitude')
+for i = 1:length(AV)
+    Freq = FreqV(1);
+    Ph = PhV(1);
+    A = AV(i);
+    a = sim('Lab_4');
+    plotDataTiled(a.sim_in,a.sim_out,'k','Zmiana amplitudy','Sygnal wejsciowy','Sygnal wyjsciowy');
+end
+
+
+
 
 
 
 %Zadanie 4. Zamodelowany filtr
 A=1;
 Ph=0;
-Cutoff = sqrt(length('Forostianyi')^2 + length('Bohdan')^2);
+Cutoff = sqrt(25 + 81); % Illia Bannitsyn 5 i 9 liter => 25 + 81
 % Cutoff frequency for low-pass RC filter = 1/RC. => R * C = 0.1 * Cutoff
-R = 0.1 * Cutoff;
+R = 1/Cutoff;
 C = 1;
-Numerator=[1];
+Numerator=[0 1];
 Denominator=[R*C 1];
 
 disp('Frequency lower, than cutoff.Amplitude should be the same')
